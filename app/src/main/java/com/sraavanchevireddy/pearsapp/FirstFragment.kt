614860e89b36103dc2524bb36fa558756a113ba9
Sraavan Chevireddy <schevireddy.ironman@outlook.com>
2023-01-16 20:30:05 +0530

    Initial Commit

---
+++ app/src/main/java/com/sraavanchevireddy/pearsapp/FirstFragment.kt
@@ -0,0 +1,55 @@
+package com.sraavanchevireddy.pearsapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import com.sraavanchevireddy.pearsapp.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        binding.button.setOnClickListener {
+            val bundle = bundleOf("hindi_poems" to String)
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, bundle)
+        }
+        binding.button2.setOnClickListener {
+
+        }
+        binding.button3.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
